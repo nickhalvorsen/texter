97dc85d474cac03a8660b631166d09149dbca775
Nick Halvorsen <nhalvorsen@BRIOSOLUTIONS.local>
2018-10-25 11:59:00 -0400

    works with html

--- texter.js
+++ texter.js
@@ -5,8 +5,10 @@ class Texter {
     }
 
     display(text, delayMs, callback) {
-        this.totalString = text
+        this.message = text
+        this.index = 0
         this.currentString = ''
+        this.closingTags = ''
         this.delayMs = delayMs
         this.callback = callback
 
@@ -14,16 +16,54 @@ class Texter {
     }
 
     _updateTexter() {
-        if (this.currentString.length < this.totalString.length) {
-            this.currentString += this.totalString[this.currentString.length]
-            this.elem.innerHTML = this.currentString
+        if (this.index < this.message.length) {
+            var useDelay = true
 
-            window.setTimeout(() => this._updateTexter(), this.delayMs);
+            var letterToAdd = this.message[this.index]
+
+            if (letterToAdd == '<' && this.message[this.index+1] == '/') {
+                var start = this.index
+                var end = this.message.indexOf('>', start) 
+                var htmlTag = this.message.substring(start, end+1)
+                var closingTag = '</' + this._getTagName(htmlTag) + '>'
+
+                this.currentString += htmlTag
+                this.index += closingTag.length 
+                this.closingTags = this.closingTags.replace('/^' + closingTag + '/')
+                useDelay = false
+            }
+            else if (letterToAdd == '<') {
+                var start = this.index
+                var end = this.message.indexOf('>', start) 
+                var htmlTag = this.message.substring(start, end+1)
+                var closingTag = '</' + this._getTagName(htmlTag) + '>'
+
+                this.currentString += htmlTag
+                this.index += htmlTag.length 
+                this.closingTags = closingTag + this.closingTags
+                useDelay = false
+            }
+
+            else {
+                this.currentString += letterToAdd
+                this.index++
+            }
+
+            this.elem.innerHTML = this.currentString + this.closingTags
+
+            var delayMs = useDelay ? this.delayMs : 0
+            window.setTimeout(() => this._updateTexter(), delayMs);
         }
         else {
             if (this.callback !== undefined) {
                 this.callback();
             }
         }
+    }
+
+    _getTagName(tag) {
+        var regex = /<\/?(\w+)/
+        var match = regex.exec(tag)
+        return match[1]
     }
 }
